04839bdf5a8ed9237da6ae1774935d740eb262cf
Denis Strelkov <ds@planado.ru>
2019-04-29 18:30:33 +0300

    Bump deps

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
     }]
   },
   plugins: [
-    new CleanWebpackPlugin(["build"]),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src/index.html"),
     }),
