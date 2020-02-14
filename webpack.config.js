dfa17780580990c1f53f7f570dc5329423ada1b2
Denis Strelkov <ds@planado.ru>
2020-02-14 13:45:21 +0700

    Update clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const path = require("path");
 
